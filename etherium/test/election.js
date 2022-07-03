81b3682b6d8917b9d9f1b441503ba796f04947f9
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-07-03 20:02:25 +0300

    SM: abstracted to separate dir

--- etherium/test/election.js
+++ etherium/test/election.js
@@ -10,20 +10,20 @@ contract('Election', () => {
   it('initializes candidate 1 with the correct values',
     () => Election.deployed()
                   .then((instance) => instance.candidates(1))
-                  .then((candidateOne) => {
-                    assert.equal(candidateOne.id, 1, 'contains the correct id');
-                    assert.equal(candidateOne.name, 'Candidate 1', 'contains the correct name');
-                    assert.equal(candidateOne.voteCount, 0, 'contains the correct votes count');
+                  .then(({id, name, voteCount}) => {
+                    assert.equal(id, 1, 'contains the correct id');
+                    assert.equal(name, 'Candidate 1', 'contains the correct name');
+                    assert.equal(voteCount, 0, 'contains the correct votes count');
                   })
   );
   
   it('initializes candidate 2 with the correct values',
     () => Election.deployed()
                   .then((instance) => instance.candidates(2))
-                  .then((candidateTwo) => {
-                    assert.equal(candidateTwo.id, 2, 'contains the correct id');
-                    assert.equal(candidateTwo.name, 'Candidate 2', 'contains the correct name');
-                    assert.equal(candidateTwo.voteCount, 0, 'contains the correct votes count');
+                  .then(({id, name, voteCount}) => {
+                    assert.equal(id, 2, 'contains the correct id');
+                    assert.equal(name, 'Candidate 2', 'contains the correct name');
+                    assert.equal(voteCount, 0, 'contains the correct votes count');
                   })
   );
 });
