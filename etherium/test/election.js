76d71cf600a21c9faae1427980993e5b3cc951db
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-07-04 15:32:19 +0300

    Code quality: eslint, prettier, husky, lintstaged, plugins; reformat js files

--- etherium/test/election.js
+++ etherium/test/election.js
@@ -1,29 +1,28 @@
 const Election = artifacts.require('Election');
 
 contract('Election', () => {
-  it('initializes with 2 candidates',
-    () => Election.deployed()
-                  .then((instance) => instance.candidatesCount())
-                  .then((count) => { assert.equal(count, 2) })
-  );
-  
-  it('initializes candidate 1 with the correct values',
-    () => Election.deployed()
-                  .then((instance) => instance.candidates(1))
-                  .then(({id, name, voteCount}) => {
-                    assert.equal(id, 1, 'contains the correct id');
-                    assert.equal(name, 'Candidate 1', 'contains the correct name');
-                    assert.equal(voteCount, 0, 'contains the correct votes count');
-                  })
-  );
-  
-  it('initializes candidate 2 with the correct values',
-    () => Election.deployed()
-                  .then((instance) => instance.candidates(2))
-                  .then(({id, name, voteCount}) => {
-                    assert.equal(id, 2, 'contains the correct id');
-                    assert.equal(name, 'Candidate 2', 'contains the correct name');
-                    assert.equal(voteCount, 0, 'contains the correct votes count');
-                  })
-  );
+  it('initializes with 2 candidates', () =>
+    Election.deployed()
+      .then((instance) => instance.candidatesCount())
+      .then((count) => {
+        assert.equal(count, 2);
+      }));
+
+  it('initializes candidate 1 with the correct values', () =>
+    Election.deployed()
+      .then((instance) => instance.candidates(1))
+      .then(({ id, name, voteCount }) => {
+        assert.equal(id, 1, 'contains the correct id');
+        assert.equal(name, 'Candidate 1', 'contains the correct name');
+        assert.equal(voteCount, 0, 'contains the correct votes count');
+      }));
+
+  it('initializes candidate 2 with the correct values', () =>
+    Election.deployed()
+      .then((instance) => instance.candidates(2))
+      .then(({ id, name, voteCount }) => {
+        assert.equal(id, 2, 'contains the correct id');
+        assert.equal(name, 'Candidate 2', 'contains the correct name');
+        assert.equal(voteCount, 0, 'contains the correct votes count');
+      }));
 });
