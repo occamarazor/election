3fe59fe8eedb05c84848e70126748673d9eb7222
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-07-08 15:20:10 +0300

    Truffle: async/await tests

--- truffle/test/election.js
+++ truffle/test/election.js
@@ -1,30 +1,27 @@
 const Election = artifacts.require('Election');
 
 contract('Election', () => {
-  it('initializes with 2 candidates', () =>
-    Election.deployed()
-      .then((instance) => instance.candidatesCount())
-      .then((count) => {
-        assert.equal(count, 2);
-      }));
+  it('initializes with 2 candidates', async () => {
+    const electionInstance = await Election.deployed();
+    const candidatesCount = await electionInstance.candidatesCount();
+    assert.equal(candidatesCount, 2);
+  });
 
-  it('initializes candidate 1 with the correct values', () =>
-    Election.deployed()
-      .then((instance) => instance.candidates(1))
-      .then(({ id, name, slogan, voteCount }) => {
-        assert.equal(id, 1, 'contains the correct id');
-        assert.equal(name, 'Franklin', 'contains the correct name');
-        assert.equal(slogan, 'Happy Days Are Here Again', 'contains the correct slogan');
-        assert.equal(voteCount, 0, 'contains the correct votes count');
-      }));
+  it('initializes the candidate 1 with correct values', async () => {
+    const electionInstance = await Election.deployed();
+    const { id, name, slogan, voteCount } = await electionInstance.candidates(1);
+    assert.equal(id, 1, 'contains the correct id');
+    assert.equal(name, 'Franklin', 'contains the correct name');
+    assert.equal(slogan, 'Happy Days Are Here Again', 'contains the correct slogan');
+    assert.equal(voteCount, 0, 'contains the correct votes count');
+  });
 
-  it('initializes candidate 2 with the correct values', () =>
-    Election.deployed()
-      .then((instance) => instance.candidates(2))
-      .then(({ id, name, slogan, voteCount }) => {
-        assert.equal(id, 2, 'contains the correct id');
-        assert.equal(name, 'Grant', 'contains the correct name');
-        assert.equal(slogan, 'Let Us Have Peace', 'contains the correct slogan');
-        assert.equal(voteCount, 0, 'contains the correct votes count');
-      }));
+  it('initializes the candidate 2 with correct values', async () => {
+    const electionInstance = await Election.deployed();
+    const { id, name, slogan, voteCount } = await electionInstance.candidates(2);
+    assert.equal(id, 2, 'contains the correct id');
+    assert.equal(name, 'Grant', 'contains the correct name');
+    assert.equal(slogan, 'Let Us Have Peace', 'contains the correct slogan');
+    assert.equal(voteCount, 0, 'contains the correct votes count');
+  });
 });
