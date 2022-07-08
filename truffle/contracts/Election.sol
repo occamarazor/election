3af459b45a6dda7b4c1e1b0a40f3c2e1db0219cd
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-07-08 15:19:51 +0300

    Truffle: vote for candidate

--- truffle/contracts/Election.sol
+++ truffle/contracts/Election.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 // Declare the source file compiler version
 pragma solidity ^0.8.1;
 
@@ -9,12 +10,14 @@ contract Election {
         string slogan;
         uint voteCount;
     }
+    // Store voted accounts
+    mapping (address => bool) public voters;
     // Store candidates
     mapping (uint => Candidate) public candidates;
     // Store candidates count
     uint public candidatesCount;
     // Constructor
-    constructor() public {
+    constructor() {
         addCandidate("Franklin", "Happy Days Are Here Again");
         addCandidate("Grant", "Let Us Have Peace");
     }
@@ -22,5 +25,10 @@ contract Election {
     function addCandidate(string memory _name, string memory _slogan) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, _slogan, 0);
+    }
+    // Vote for candidate
+    function vote(uint _candidateId) public {
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
     }
 }
